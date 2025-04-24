b14e7a372ac64d22a70fa68ce112c62d008f03d2
KdauntlN <henryknzed@gmail.com>
2025-04-24 21:40:58 +1000

    turned into doom no idea why

--- src/main.c
+++ src/main.c
@@ -1,25 +1,7 @@
 #include <raylib.h>
-
-int scene = 0;
-
-Vector2 trianglePoints[] = {{380, 205}, {380, 245}, {420, 225}};
+#include <rcamera.h>
 
 int main() {
-    InitWindow(800, 600, "Game");
-    SetExitKey(0);
 
-    while (!WindowShouldClose()) {
-        ClearBackground(RAYWHITE);
-        BeginDrawing();
-        switch (scene) {
-            default:
-                CloseWindow();
-            case 0:
-                DrawText("SKYBOUND", 400 - (MeasureText("SKYBOUND", 60) / 2), 100, 60, BLACK);
-                DrawTriangle(trianglePoints[0], trianglePoints[1], trianglePoints[2], RED);
-        }
-            EndDrawing();
-    }
-    CloseWindow();
     return 0;
 }

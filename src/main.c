10a6ef73f4642d222cc79e7f67e755d740361d1d
KdauntlN <henryknzed@gmail.com>
2025-04-24 21:38:57 +1000

    Turned this into DOOM, not sure why

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <raylib.h>
 
+int scene = 0;
+
+Vector2 trianglePoints[] = {{380, 205}, {380, 245}, {420, 225}};
+
 int main() {
     InitWindow(800, 600, "Game");
     SetExitKey(0);
@@ -7,7 +11,14 @@ int main() {
     while (!WindowShouldClose()) {
         ClearBackground(RAYWHITE);
         BeginDrawing();
-        EndDrawing();
+        switch (scene) {
+            default:
+                CloseWindow();
+            case 0:
+                DrawText("SKYBOUND", 400 - (MeasureText("SKYBOUND", 60) / 2), 100, 60, BLACK);
+                DrawTriangle(trianglePoints[0], trianglePoints[1], trianglePoints[2], RED);
+        }
+            EndDrawing();
     }
     CloseWindow();
     return 0;
